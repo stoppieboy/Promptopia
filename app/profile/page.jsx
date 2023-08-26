7f6d37c029b004559ef76a12ce69fe5906e60da9
Shivam <shivamv1332@gmail.com>
2023-08-26 21:36:41 +0530

    second commit

---
+++ app/profile/page.jsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { useState,useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+import Profile from "@/components/Profile"
+
+const MyProfile = () => {
+
+    const { data: session } = useSession();
+    const [ posts, setPosts ] = useState([]);
+    const router = useRouter();
+
+    useEffect(()=>{
+        const fetchPosts = async() => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+          const data = await response.json();
+          setPosts(data);
+        }
+        if(session?.user.id) fetchPosts();
+    },[])
+
+    const handleDelete = async(post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+        if(hasConfirmed){
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`,{
+                    method: 'DELETE',
+                });
+
+                const filteredPost = posts.filter((p) => p._id !== post._id)
+                setPosts(filteredPost);
+            } catch (error) {
+                
+            }
+        }
+    }
+
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+
+    return (
+        <Profile
+            name='My'
+            desc="Welcome to your personlized profile page"
+            data={posts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
+    )
+}
+
+export default MyProfile
